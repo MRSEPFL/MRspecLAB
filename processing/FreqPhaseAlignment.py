52ed8a0f51e1155e2e18709b075c0acb9bd8530f
Y99dr451l <matthias@kockisch.eu>
2023-10-22 01:59:52 +0200

    implemented freq and phase alignment module

---
+++ processing/FreqPhaseAlignment.py
@@ -0,0 +1,57 @@
+import ProcessingStep as ps
+import suspect
+import numpy as np
+
+class FreqPhaseAlignment(ps.ProcessingStep):
+    def __init__(self):
+        super().__init__({"freqRange": None, "median": False})
+    
+    def process(self, data):
+        if (type(self.parameters["freqRange"]) is not tuple) or len(self.parameters["freqRange"]) != 2: freqRange = None
+        else: freqRange = self.parameters["freqRange"]
+        if self.parameters["median"]:
+            target = np.median(data, axis=0)
+            target = data[0].inherit(target)
+        else:
+            target = data[0]
+        freqShifts = []
+        phaseShifts = []
+        output = []
+        for d in data:
+            # output.append(suspect.processing.frequency_correction.correct_frequency_and_phase(d, data[0]))
+            freqShift, phaseShift = suspect.processing.frequency_correction.spectral_registration(d, target, frequency_range=freqRange)
+            freqShifts.append(freqShift)
+            phaseShifts.append(phaseShift)
+            output.append(d.adjust_frequency(-freqShift).adjust_phase(-phaseShift))
+        self.freqShifts = freqShifts
+        self.phaseShifts = phaseShifts
+        self.inputData = data
+        self.processedData = output
+        return self.processedData
+
+    def plot(self, canvas):
+        canvas.figure.suptitle(self.__class__.__name__)
+        ax = canvas.figure.add_subplot(2, 2, 1)
+        for d in self.inputData:
+            ax.plot(d.frequency_axis_ppm(), d.spectrum())
+        ax.set_xlabel('Frequency (ppm)')
+        ax.set_ylabel('Amplitude')
+        ax.set_title("Input")
+        ax = canvas.figure.add_subplot(2, 2, 2)
+        for d in self.processedData:
+            ax.plot(d.frequency_axis_ppm(), d.spectrum())
+        ax.set_xlabel('Frequency (ppm)')
+        ax.set_ylabel('Amplitude')
+        ax.set_title("Output")
+        ax = canvas.figure.add_subplot(2, 2, 3)
+        ax.plot(self.freqShifts)
+        ax.set_xlabel('Index')
+        ax.set_ylabel('Frequency shift (Hz)')
+        ax.set_title("Frequency shifts")
+        ax = canvas.figure.add_subplot(2, 2, 4)
+        ax.plot(self.phaseShifts)
+        ax.set_xlabel('Index')
+        ax.set_ylabel('Phase shift (rad)')
+        ax.set_title("Phase shifts")
+        canvas.figure.tight_layout()
+        canvas.draw()
--- processing/basic.py
+++ processing/basic.py
@@ -1,5 +1,4 @@
 import ProcessingStep as ps
-import suspect
 import numpy as np
 
 class Average(ps.ProcessingStep):
@@ -14,24 +13,3 @@ class Average(ps.ProcessingStep):
         output = temp.inherit(output) # retrieve suspect metadata
         self.processedData = [output]
         return self.processedData
-
-class FreqAlignment(ps.ProcessingStep):
-    def process(self, data):
-        output = [data[0]]
-        for d in data:
-            output.append(suspect.processing.frequency_correction.correct_frequency_and_phase(d, data[0]))
-        self.processedData = output
-        return self.processedData
-    
-    def plot(self, canvas):
-        ax = canvas.figure.add_subplot(1, 2, 1)
-        ax.plot(self.processedData[0].time_axis(), abs(self.processedData[0]))
-        ax.set_xlabel('Time (s)')
-        ax.set_ylabel('Signal Intensity')
-        ax.set_title("Reference")
-        ax = canvas.figure.add_subplot(1, 2, 2)
-        ax.plot(self.processedData[1].time_axis(), abs(self.processedData[1]))
-        ax.set_xlabel('Time (s)')
-        ax.set_ylabel('Signal Intensity')
-        ax.set_title("Aligned")
-        canvas.draw()
