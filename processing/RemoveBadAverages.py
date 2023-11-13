fae751d2745bf399b968ae133512609fd763d2a8
Y99dr451l <matthias@kockisch.eu>
2023-11-13 18:05:13 +0100

    updated plots of removebadaverages

--- processing/RemoveBadAverages.py
+++ processing/RemoveBadAverages.py
@@ -20,6 +20,29 @@ class RemoveBadAverages(ps.ProcessingStep):
             for d in data["input"]: metric = np.sum((s - ref)**2 for s in specs)
         self.zscores = (metric - np.mean(metric)) / np.std(metric)
         mask = np.abs(self.zscores) < self.parameters["stdDevThreshold"]
+        self.removed = []
         for i, d in enumerate(data["input"]):
             if mask[i]: output.append(d)
+            else: self.removed.append(i)
         data["output"] = output
+
+    def plot(self, canvas, data):
+        ax = canvas.figure.add_subplot(2, 1, 1)
+        for i, d in enumerate(data["input"]):
+            if i in self.removed: colour = "red"
+            else: colour = (0, 0, 0, 1/len(data["input"]))
+            ax.plot(d.time_axis(), d, c=colour)
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Intensity')
+        ax.set_title("Removed data: " + str(self.removed))
+        ax = canvas.figure.add_subplot(2, 1, 2)
+        for i, d in enumerate(data["input"]):
+            if i in self.removed: colour = "red"
+            else: colour = (0, 0, 0, 1/len(data["input"]))
+            ax.plot(d.frequency_axis_ppm(), d.spectrum(), c=colour)
+        ax.set_xlabel('Chemical shift (ppm)')
+        ax.set_ylabel('Amplitude')
+        ax.set_xlim((np.max(d.frequency_axis_ppm()), np.min(d.frequency_axis_ppm())))
+        canvas.figure.suptitle(self.__class__.__name__ + " (domain: " + self.parameters["domain"] + ")")
+        canvas.figure.tight_layout()
+        canvas.draw()
