c6479921a3a6e6d1afbcbc4440f0396a0fb485d6
Y99dr451l <matthias@kockisch.eu>
2023-11-16 21:21:59 +0100

    compacted main

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# import os
-# import glob
-# import inspect
-# import importlib.util
-
-
-import wx
 from interface.GUI import MyApp
-
-
-
 
 if __name__ == "__main__":
     app = MyApp(0)
     app.MainLoop()
-
-
