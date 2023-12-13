8e5696941cfd0b3b4a930de18779a4fc7d1df68e
Ca-robin <62765455+Ca-robin@users.noreply.github.com>
2023-12-13 14:00:48 +0100

    panel change parameters pipeline added(not working yet)

--- interface/pipeline_window.py
+++ interface/pipeline_window.py
@@ -1,5 +1,6 @@
 from . import PipelineNodeGraph
 from GimelStudio.nodegraph_dnd import NodeGraphDropTarget
+from GimelStudio.nodeproperties_pnl import NodePropertiesPanel
 
 import wx
 
@@ -10,10 +11,18 @@ class PipelineWindow(wx.Frame):
         super(PipelineWindow, self).__init__(*args, **kw)
 
         self.SetSize(wx.Size(1200, 500))
+        self.mainpanel = wx.Panel(self, wx.ID_ANY)
+        self.pipelinesizer= wx.BoxSizer(wx.HORIZONTAL)
+        self.mainpanel.SetSizer(self.pipelinesizer)
 
-
-        self.pipelinePanel  = PipelineNodeGraph.NodeGraphPanel(self, size=(100, 100))
+        self.pipelinePanel  = PipelineNodeGraph.NodeGraphPanel(self.mainpanel, size=(100, 100))
         self.pipelinePanel.SetDropTarget(NodeGraphDropTarget(self.pipelinePanel))
+        
+        self.prop_pnl = NodePropertiesPanel(self.mainpanel,idname="PROPERTIES_PNL", menu_item=None,size=(360, 500))
+        
+        
+        self.pipelinesizer.Add(self.pipelinePanel, 1, wx.ALL | wx.EXPAND, 5)
+        self.pipelinesizer.Add(self.prop_pnl, 0, wx.ALL | wx.EXPAND, 5)
         
         self.Bind(wx.EVT_CLOSE, self.on_close)
         self.SetIcon(wx.Icon("resources/icon_32p.png"))
