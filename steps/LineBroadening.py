92511e109f7acd14ae0c09520b24d6c2a752ef03
Y99dr451l <matthias@kockisch.eu>
2024-02-01 20:57:51 +0100

    fixed plot arguments

--- steps/LineBroadening.py
+++ steps/LineBroadening.py
@@ -16,19 +16,20 @@ class LineBroadening(ProcessingStep):
             self.dmax = max(self.dmax, np.max(d)) # for plotting exp
         data["output"] = output
 
-    def plot(self, canvas, data):
-        canvas.figure.suptitle(self.__class__.__name__)
-        ax = canvas.figure.add_subplot(2, 1, 1)
+    def plot(self, figure, data):
+        figure.suptitle(self.__class__.__name__)
+        ax = figure.add_subplot(2, 1, 1)
         for d in data["input"]:
             ax.plot(d.time_axis(), d)
         ax.plot(d.time_axis(), self.exp * self.dmax, ':k')
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Intensity')
         ax.set_title("Input and apodisation function")
-        ax = canvas.figure.add_subplot(2, 1, 2)
+        ax = figure.add_subplot(2, 1, 2)
         for d in data["output"]:
             ax.plot(d.time_axis(), d)
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Intensity')
         ax.set_title("Output")
-        canvas.figure.tight_layout()
+        figure.tight_layout()
+
