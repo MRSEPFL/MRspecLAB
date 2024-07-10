320cc676400058fb0713638d5aa79040c25de58c
Y99dr451l <matthias@kockisch.eu>
2024-07-10 19:02:24 +0200

    added basic standalone file viewer

---
+++ MRSviewer.py
@@ -0,0 +1,18 @@
+import os, sys, wx
+from interface.file_panel import FilePanel
+
+class MainApp(wx.App):
+    def OnInit(self):
+        self.frame = wx.Frame(None, title="MRS Viewer")
+        self.panel = FilePanel(self.frame)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.panel, 1, wx.EXPAND, 0)
+        self.frame.SetSizer(self.sizer)
+        self.frame.Layout()
+        self.SetTopWindow(self.frame)
+        self.frame.Show()
+        return True
+    
+if __name__ == "__main__":
+    app = MainApp(0)
+    app.MainLoop()
