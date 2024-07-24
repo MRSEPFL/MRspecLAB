added automatic averaging for multi-shot sequences, fixed control file reading

processing/processingPipeline.py
@@ -1,4 +1,5 @@
 import wx
+import numpy as np
 import os, sys, shutil, zipfile, time, subprocess
 import matplotlib
 from suspect.io.lcmodel import write_all_files
@@ -61,8 +62,7 @@ def loadInput(self):
         except: utils.log_warning("Error loading water reference: " + wrefpath + "\n\t" + str(sys.exc_info()[0]))
         else:
             if self.originalWref is None: utils.log_warning("Couldn't load water reference: " + wrefpath)
-            self.originalWref = self.originalWref[0]
-            utils.log_debug("Loaded water reference: " + filepath)
+            else: utils.log_debug("Loaded water reference: " + filepath)
 
     utils.log_info(len(self.originalData), " MRS files and ", "no" if self.originalWref is None else "1", " water reference file loaded")
 
@@ -131,7 +131,7 @@ def processStep(self, step, nstep):
     dataDict["input"] = self.dataSteps[-1]
     dataDict["wref"] = self.wrefSteps[-1]
     dataDict["output"] = []
-    dataDict["wref_output"] = None
+    dataDict["wref_output"] = []
     
     self.button_step_processing.Disable()
     if not self.fast_processing:
@@ -142,7 +142,7 @@ def processStep(self, step, nstep):
     step.process(dataDict)
     utils.log_info("Time to process " + step.__class__.__name__ + ": {:.3f}".format(time.time() - start_time))
     self.dataSteps.append(dataDict["output"])
-    if dataDict["wref_output"] is not None:
+    if len(dataDict["wref_output"]) != 0:
         self.wrefSteps.append(dataDict["wref_output"])
     else: self.wrefSteps.append(dataDict["wref"])
 
@@ -170,7 +170,7 @@ def processStep(self, step, nstep):
         filepath = os.path.join(steppath, "data")
         if not os.path.exists(filepath): os.mkdir(filepath)
         for i, d in enumerate(dataDict["output"]): save_raw(os.path.join(filepath, str(i) + ".RAW"), d, seq=self.sequence)
-        save_raw(os.path.join(filepath, "wref.RAW"), self.wrefSteps[-1], seq=self.sequence)
+        for i, d in enumerate(dataDict["wref_output"]): save_raw(os.path.join(filepath, "wref_" + str(i) + ".RAW"), d, seq=self.sequence)
     # canvas plot
     if not self.fast_processing:
         self.matplotlib_canvas.clear()
@@ -196,7 +196,7 @@ def saveDataPlot(self):
         
 def analyseResults(self):
     results = self.dataSteps[-1]
-    wresult = self.wrefSteps[-1]
+    wresult = self.wrefSteps[-1][0].inherit(np.mean(np.array(self.wrefSteps[-1]), 0))
     self.basisfile = None
 
     # basis file
@@ -235,7 +235,7 @@ def analyseResults(self):
             self.basisfile = None
         else: self.basisfile = self.basisfile_user
 
-    if self.basisfile is None and os.path.exists(basisfile_gen): # generated basis file
+    if self.basisfile is None and basisfile_gen is not None and os.path.exists(basisfile_gen): # generated basis file
         dlg = wx.MessageDialog(None, basisfile_gen, "Basis set found, is it the right one?\n" + basisfile_gen, wx.YES_NO | wx.CANCEL | wx.ICON_INFORMATION)
         button_clicked = dlg.ShowModal()
         if button_clicked == wx.ID_YES: self.basisfile = basisfile_gen
@@ -298,6 +298,7 @@ def analyseResults(self):
             "FILCSV": f"./{label}.csv",
             "FILCOO": f"./{label}.coord",
             "FILPS": f"./{label}.ps",
+            "FILTAB": f"./{label}.table",
             # "FILRAW": f"./{label}.RAW"
             "DOWS": wresult is not None,
             "NUNFIL": result.np,

steps/EddyCurrentCorrection.py
@@ -23,29 +23,29 @@ class EddyCurrentCorrection(ProcessingStep):
         super().__init__(nodegraph, id)
 
     def process(self, data):
-        if data["wref"] is None:
+        if len(data["wref"]) == 0:
             data["output"] = data["input"]
             return
         w = self.get_parameter("gaussian_width")
         window = np.linspace(-3, 3, w)
         window = np.exp(-window**2)
         window /= np.sum(window)
-        self.wphase = np.unwrap(np.angle(data["wref"]))
+        self.wphase = np.unwrap(np.angle(data["wref"][0]))
         self.wphasesmooth = np.convolve(self.wphase, window, mode="same")
         window = len(self.wphase) // 10
-        poly = np.polynomial.polynomial.Polynomial.fit(data["wref"].time_axis()[:window], self.wphasesmooth[:window], 1)
+        poly = np.polynomial.polynomial.Polynomial.fit(data["wref"][0].time_axis()[:window], self.wphasesmooth[:window], 1)
         coef = poly.convert().coef
-        self.wphasefit = coef[1] * data["wref"].time_axis() + coef[0]
+        self.wphasefit = coef[1] * data["wref"][0].time_axis() + coef[0]
         ecc = np.exp(-1j * (self.wphasesmooth - self.wphasefit))
         output = []
         for d in data["input"]: output.append(d * ecc)
-        ecwref = data["wref"] * ecc
+        ecwref = data["wref"][0] * ecc
         data["output"] = output
-        data["wref_output"] = ecwref
+        data["wref_output"] = [ecwref]
     
     def plot(self, figure, data):
         figure.suptitle(self.__class__.__name__)
-        if data["wref"] is None:
+        if len(data["wref"]) == 0:
             ax = figure.add_subplot(1, 1, 1)
             for d in data["output"]:
                 ax.plot(d.frequency_axis_ppm(), np.real(d.spectrum()))
@@ -69,10 +69,10 @@ class EddyCurrentCorrection(ProcessingStep):
         ax.set_title("Output")
         ax.set_xlim((np.max(d.frequency_axis_ppm()), np.min(d.frequency_axis_ppm())))
         ax = figure.add_subplot(2, 2, (3, 4))
-        ax.plot(data["wref"].time_axis(), self.wphase, "-k", label="original phase")
-        ax.plot(data["wref"].time_axis(), self.wphasesmooth, ":r", label="smoothed phase")
-        ax.plot(data["wref"].time_axis(), self.wphasefit, ":k", label="linear fit to smoothed phase")
-        ax.plot(data["wref_output"].time_axis(), np.unwrap(np.angle(data["wref_output"])), "-b", label="corrected phase\n= original - (smoothed - fit)")
+        ax.plot(data["wref"][0].time_axis(), self.wphase, "-k", label="original phase")
+        ax.plot(data["wref"][0].time_axis(), self.wphasesmooth, ":r", label="smoothed phase")
+        ax.plot(data["wref"][0].time_axis(), self.wphasefit, ":k", label="linear fit to smoothed phase")
+        ax.plot(data["wref_output"][0].time_axis(), np.unwrap(np.angle(data["wref_output"][0])), "-b", label="corrected phase\n= original - (smoothed - fit)")
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Phase (rad)')
         ax.legend()

steps/_CoilCombinationAdaptive.py
@@ -1,6 +1,6 @@
 # this file exists to allow coil combination in MRSviewer without also including the nodegraph libraries
 import numpy as np
-from processing.processing_helpers import zero_phase_flip
+from interface import utils
 
 # translated from FID-A code
 def estimate_csm(data):
@@ -31,23 +31,27 @@ def eig_power(R):
     v = np.conj(v)
     return v, d
 
-def coil_combination_adaptive(data):
-    if data["wref"] is not None:
-        ref = data["wref"][:, 1] # 2nd timepoints of all coils of the water reference
-    else: ref = data["input"][0][:, 1] # 2nd timepoints of all coils of the first fid
+def coil_combination_adaptive(data, p=0):
+    if p == 0: p = data["input"][0].metadata["ave_per_rep"]
+    ref = "wref" if len(data["wref"]) != 0 else "input"
+    if ref == "input": utils.log_warning("No water reference provided, using averaged FIDs as reference")
+    ref = np.mean(np.array(data[ref]), 0)[:, 0]
     phase = np.exp(-1j*np.angle(ref))
     csm = np.array(estimate_csm(ref * phase)[0][0])
     csmsq = np.sum(csm * np.conj(csm), 0)
     csm[csm < np.finfo(float).eps] = 1
+    output = []
     for d in data["input"]:
-        output = []
+        output2 = []
         for i in range(0, d.shape[-1]):
-            output.append(np.sum(np.conj(csm) * d[:, i] * phase, 0) / csmsq)
-        data["output"].append(d.inherit(np.array(output)))
-    data["output"] = zero_phase_flip(data["output"])
-    if data["wref"] is not None:
+            output2.append(np.sum(np.conj(csm) * d[:, i] * phase, 0) / csmsq)
+        output.append(d.inherit(np.array(output2)))
+    data["output"] = [output[i].inherit(np.mean(output[i:i+p], 0)) for i in range(0, len(output), p)]
+    if len(data["wref"]) != 0:
         output = []
-        for i in range(0, data["wref"].shape[-1]):
-            output.append(np.sum(np.conj(csm) * data["wref"][:, i] * phase, 0) / csmsq)
-        data["wref_output"] = data["wref"].inherit(np.array(output))
-        data["wref_output"] = zero_phase_flip([data["wref_output"]])[0]
+        for d in data["wref"]:
+            output2 = []
+            for i in range(0, d.shape[-1]):
+                output2.append(np.sum(np.conj(csm) * d[:, i] * phase, 0) / csmsq)
+            output.append(d.inherit(np.array(output2)))
+        data["wref_output"] = [output[i].inherit(np.mean(output[i:i+p], 0)) for i in range(0, len(output), p)]